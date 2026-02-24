ff89884acb58d0713005c051f332afd0751d770e
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-02-24 11:18:22 +0100

    fixed bug

--- src/components/homeScreen.jsx
+++ src/components/homeScreen.jsx
@@ -8,7 +8,7 @@ function HomeScreen() {
         <p className="text-[rgba(255,255,255,1)] font-serif md:text-2xl md:font-extrabold font-bold text-[14px]">
           Handcrafted with passion, delivered with care.
         </p>
-        <button className="flex bg-[rgba(255,122,24,1)] w-[65%] text-[rgba(255,255,255,1)] md:w-[13%] rounded-[10px] py-4 px-4 font-serif text-center cursor-pointer hover:bg-amber-600 transition-all font-medium ease-in-out md:font-bold">
+        <button className="flex bg-[rgba(255,122,24,1)] w-[65%] text-[rgba(255,255,255,1)] md:w-[16%] rounded-[10px] py-4 px-4 font-serif text-center cursor-pointer hover:bg-amber-600 transition-all font-medium ease-in-out md:font-bold">
           Discover What's new
         </button>
       </div>
